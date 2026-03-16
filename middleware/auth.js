39834e0b7acf9e7e02546c94da8a5a77cecae768
Adam Faiq <adamfaiq62@gmail.com>
2026-03-17 01:51:59 +0800

    add adminOnly in auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -33,4 +33,12 @@ const protect = async (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+const adminOnly = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ success: false, message: "Admin only" });
+  }
+};
+
+module.exports = { protect, adminOnly };
